d3c557e44d1f8f79a68ee9e9009cd20ed678aa12
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-30 21:13:13 +0530

    ADDED:Creating TicTacToe gaming Board

--- src/Com/BridgeLabz/TicTacToe/TicTacToe.java
+++ src/Com/BridgeLabz/TicTacToe/TicTacToe.java
@@ -1,7 +1,17 @@
 package Com.BridgeLabz.TicTacToe;
 
 public class TicTacToe {
+
+    private static char[] element;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Tic Tac Toe game");
+        System.out.println("Welcome to Tic Tac Toe Game....!!!");
+        board();
+    }
+    private static void board() {
+        element = new char[10];
+        for(int i=1; i<10; i++) {
+            element[i]='.';
+        }
     }
 }
